8c2c867557eef3356e6e532579f3a59be255f29f
Asper Hsu <asperwon@gmail.com>
2017-02-25 23:35:27 +0800

    remove dotenv, replace to putenv

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,12 @@
 require("vendor/autoload.php");
 date_default_timezone_set('Asia/Taipei');
 
-putenv("APP_PATH=".__DIR__);
-
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+include("env.php");
+if( isset($env) && count($env) ){
+	foreach($env as $key => $value){
+		putenv($key . "=" . $value);
+	}
+}
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
---
+++ env.sample.php
@@ -0,0 +1,7 @@
+<?php
+$env = [
+	'APP_PATH'		=> __DIR__,
+	'GAE_BUCKET' 	=> "",
+	'EDIMAX_AIRBOX_TOKEN'	=> "",
+	'DEFAULT_ADMIN_PASSWORD' => "",
+];
